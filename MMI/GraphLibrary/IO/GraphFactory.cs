e2e42c2175847b654ec48bea8eaaaf2147a2c57a
Nils Schlicher <Nils.Schlicher@alumni.fh-aachen.de>
2018-04-15 16:00:04 +0200

    Beispiele für A1 hinzufügen

--- MMI/GraphLibrary/IO/GraphFactory.cs
+++ MMI/GraphLibrary/IO/GraphFactory.cs
@@ -8,10 +8,14 @@ namespace GraphLibrary.IO
 {
     /// <summary>
     /// Factory-Klasse zur Erstellung eines Graphen-Objektes.
-    /// Dabei können verschiedene Quellen genutzt werden, z.B. eine Zeichenfolge in Form einer Adjazenzmatrik oder Adjazenzliste.
+    /// Dabei können verschiedene Quellen genutzt werden, z.B. eine Zeichenfolge in Form einer Adjazenzmatrix oder Adjazenzliste.
     /// Zudem wird angegeben, ob es sich um einen gerichteten oder ungerichteten Graphen handelt, worauf die Quelle aber nicht geprüft wird.
     /// </summary>
     public class GraphFactory
     {
+
+        //Aspekte: Directed oder nicht, From String, FromFile, AdjazenzListe oder Matrix
+
+
     }
 }
