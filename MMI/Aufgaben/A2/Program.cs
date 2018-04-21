b79c95f80319a4f3da374bd0fd50e3a50863d3e7
Nils Schlicher <Nils.Schlicher@alumni.fh-aachen.de>
2018-04-21 12:14:15 +0200

    Kruskal mit Gruppen in Dictionaries verbesser, sodass immer die kleiner Gruppe zur größeren hinzugefügt wird. Zweite Variante mit DisJointSet
    3 Varianten von Prim (meine mit Lin. Suche, Heap (andere Ergebnisse) und PriorityQueue)

--- MMI/Aufgaben/A2/Program.cs
+++ MMI/Aufgaben/A2/Program.cs
@@ -13,54 +13,145 @@ namespace A2
 {
     class Program
     {
+        /// <summary>
+        /// Informationen für einen Testlauf in dieser Aufgabe
+        /// </summary>
+        public class TestData
+        {
+            public string Name { get; set; }
+            public string File { get; set; }
+        }
+
+
         static void Main(string[] args)
         {
+            /*
+            Graph gSimple = new Graph("Test1", false);
 
-            Graph g = new Graph("Test1", false);
-
-            g.AddVertex("1");
-            g.AddVertex("2");
-            g.AddVertex("3");
-            g.AddVertex("4");
-            g.AddVertex("5");
-
-
-            g.AddEdge(g.Vertices["1"], g.Vertices["2"], new Dictionary<string, double> { { "C", 13 } });
-            g.AddEdge(g.Vertices["1"], g.Vertices["3"], new Dictionary<string, double> { { "C", 4 } });
-            g.AddEdge(g.Vertices["1"], g.Vertices["5"], new Dictionary<string, double> { { "C", 9 } });
-            g.AddEdge(g.Vertices["3"], g.Vertices["2"], new Dictionary<string, double> { { "C", 18 } });
-            g.AddEdge(g.Vertices["3"], g.Vertices["4"], new Dictionary<string, double> { { "C", 7 } });
-            g.AddEdge(g.Vertices["3"], g.Vertices["5"], new Dictionary<string, double> { { "C", 10 } });
-            g.AddEdge(g.Vertices["2"], g.Vertices["4"], new Dictionary<string, double> { { "C", 12 } });
-            g.AddEdge(g.Vertices["4"], g.Vertices["5"], new Dictionary<string, double> { { "C", 5 } });
+            gSimple.AddVertex("1");
+            gSimple.AddVertex("2");
+            gSimple.AddVertex("3");
+            gSimple.AddVertex("4");
+            gSimple.AddVertex("5");
 
 
-            var a = MST.Prim(g, "C");
+            gSimple.AddEdge(gSimple.Vertices["1"], gSimple.Vertices["2"], new Dictionary<string, double> { { "C", 13 } });
+            gSimple.AddEdge(gSimple.Vertices["1"], gSimple.Vertices["3"], new Dictionary<string, double> { { "C", 4 } });
+            gSimple.AddEdge(gSimple.Vertices["1"], gSimple.Vertices["5"], new Dictionary<string, double> { { "C", 9 } });
+            gSimple.AddEdge(gSimple.Vertices["3"], gSimple.Vertices["2"], new Dictionary<string, double> { { "C", 18 } });
+            gSimple.AddEdge(gSimple.Vertices["3"], gSimple.Vertices["4"], new Dictionary<string, double> { { "C", 7 } });
+            gSimple.AddEdge(gSimple.Vertices["3"], gSimple.Vertices["5"], new Dictionary<string, double> { { "C", 10 } });
+            gSimple.AddEdge(gSimple.Vertices["2"], gSimple.Vertices["4"], new Dictionary<string, double> { { "C", 12 } });
+            gSimple.AddEdge(gSimple.Vertices["4"], gSimple.Vertices["5"], new Dictionary<string, double> { { "C", 5 } });
 
 
-            var b = MST.Kruskal(g, "C");
+            var a = MST.PrimV2(gSimple, "C");
 
+
+            var b = MST.Kruskal(gSimple, "C");
+            
 
             var costName = "Kosten";
 
-            var s = File.ReadAllText(@"SampleData\G_1_2.txt");
+            //var s = File.ReadAllText(@"SampleData\G_1_2.txt");
+            var s = File.ReadAllText(@"SampleData\G_100_200.txt");
+
             IGraph gTest = GraphFactory.GraphFromAdjListStringWithCost(s, "T1", costName, false);
 
 
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
 
-            var c = MST.Prim(gTest, costName);
+            //var c = MST.Prim(gTest, costName);
 
+            //sw.Stop();
+            //Console.WriteLine($"Prim {sw.ElapsedMilliseconds} ms with ResultCost={c.Edges.Values.Sum(x => x.Costs[costName])}");
+
+
+            sw.Restart();
+            var d1 = MST.KruskalV1(gTest, costName);
             sw.Stop();
-            Console.WriteLine($"Prim {sw.ElapsedMilliseconds} ms with ResultCost={c.Edges.Values.Sum(x => x.Costs[costName])}");
+
+            Console.WriteLine($"KruskalV1 {sw.ElapsedMilliseconds} ms with ResultCost={d1.Edges.Values.Sum(x => x.Costs[costName])}");
 
 
             sw.Restart();
             var d = MST.Kruskal(gTest, costName);
-
+            sw.Stop();
 
             Console.WriteLine($"Kruskal {sw.ElapsedMilliseconds} ms with ResultCost={d.Edges.Values.Sum(x => x.Costs[costName])}");
+
+            Console.ReadLine();
+
+            */
+
+
+
+
+
+            var costName = "Kosten";
+            List<TestData> testFiles = new List<TestData>();
+
+            //testFiles.Add(new TestData { Name = "G_1_2", File = @"SampleData\G_1_2.txt" });
+            //testFiles.Add(new TestData { Name = "G_1_20", File = @"SampleData\G_1_20.txt" });
+            //testFiles.Add(new TestData { Name = "G_1_200", File = @"SampleData\G_1_200.txt" });
+            //testFiles.Add(new TestData { Name = "G_10_20", File = @"SampleData\G_10_20.txt" });
+            testFiles.Add(new TestData { Name = "G_10_200", File = @"SampleData\G_10_200.txt" });
+            //testFiles.Add(new TestData { Name = "G_100_200", File = @"SampleData\G_100_200.txt" });
+
+
+
+            foreach (var data in testFiles)
+            {
+                Console.WriteLine($"------------------ Start Name={data.Name} ------------------");
+
+                // Initialisieren
+
+                // Einlesen in String 
+                string s = File.ReadAllText(data.File);
+
+                var swInit = System.Diagnostics.Stopwatch.StartNew();
+                IGraph g = GraphFactory.GraphFromAdjListStringWithCost(s, data.Name, costName, false);
+                swInit.Stop();
+                Console.WriteLine($"Init took {swInit.ElapsedMilliseconds} ms");
+
+                // Rechnen
+
+
+                var swCalc = System.Diagnostics.Stopwatch.StartNew();
+                var k = MST.Kruskal(g, costName);
+                swCalc.Stop();
+                Console.WriteLine($"Kruskal {swCalc.ElapsedMilliseconds} ms with ResultCost={k.Edges.Values.Sum(x => x.Costs[costName])}");
+
+                swCalc.Restart();
+                k = MST.KruskalV1(g, costName);
+                swCalc.Stop();
+                Console.WriteLine($"KruskalV1 {swCalc.ElapsedMilliseconds} ms with ResultCost={k.Edges.Values.Sum(x => x.Costs[costName])}");
+
+
+                swCalc.Restart();
+                var p = MST.Prim(g, costName);
+                swCalc.Stop();
+                Console.WriteLine($"Prim {swCalc.ElapsedMilliseconds} ms with ResultCost={p.Edges.Values.Sum(x => x.Costs[costName])}");
+
+
+                swCalc.Restart();
+                p = MST.PrimV2(g, costName);
+                swCalc.Stop();
+                Console.WriteLine($"PrimV2 {swCalc.ElapsedMilliseconds} ms with ResultCost={p.Edges.Values.Sum(x => x.Costs[costName])}");
+
+
+
+                swCalc.Restart();
+                p = MST.PrimV3(g, costName);
+                swCalc.Stop();
+                Console.WriteLine($"PrimV3 {swCalc.ElapsedMilliseconds} ms with ResultCost={p.Edges.Values.Sum(x => x.Costs[costName])}");
+                
+
+            }
+
+
+
 
             Console.ReadLine();
         }
--- MMI/GraphLibrary/Algorithm/MST.cs
+++ MMI/GraphLibrary/Algorithm/MST.cs
@@ -13,7 +13,9 @@ namespace GraphLibrary.Algorithm
     /// </summary>
     public static class MST
     {
-        public static IGraph Kruskal(IGraph graph, string costKey)
+        // Hier ist die Idee mit den Gruppen gut, aber der Haushalt damit zu aufwändig (Vereinen von zwei großen Gruppen hat zu viele Änderungen zur Auswirkung)
+        // Auch das Verbinden der Gruppen kann dynamisch besser ablaufen (kleinere in größere)
+        public static IGraph KruskalV1(IGraph graph, string costKey)
         {
             // From ist immer der Gruppengeber
             Dictionary<string, string> groupMapPerVertex = new Dictionary<string, string>();
@@ -31,7 +33,7 @@ namespace GraphLibrary.Algorithm
                 groupMap[vertex.Value.Identifier] = new List<string> { vertex.Value.Identifier };
             }
 
-            
+
 
             //sortierte Edges
             var sortedEdges = graph.Edges.Values.OrderBy(x => x.Costs[costKey]);
@@ -48,15 +50,29 @@ namespace GraphLibrary.Algorithm
                 string fromGroup = groupMapPerVertex[fromId];
                 string toGroup = groupMapPerVertex[toId];
 
+
+
                 bool wouldBeCycle = fromGroup == toGroup;
                 if (!wouldBeCycle)
                 {
+                    // von ToGroup in FromGroup
+                    // Dabei ToGroup die kleinere
+                    if (groupMap[toGroup].Count > groupMap[fromGroup].Count)
+                    {
+                        var tmp = toGroup;
+                        toGroup = fromGroup;
+                        fromGroup = tmp;
+                    }
+
+
                     // to in Gruppe von From
                     foreach (var member in groupMap[toGroup])
                     {
                         groupMapPerVertex[member] = groupMapPerVertex[fromGroup];
-                        groupMap[fromGroup].Add(member);
                     }
+
+                    // direkt ganze Liste rüber schieben
+                    groupMap[fromGroup].AddRange(groupMap[toGroup]);
 
                     //alte Gruppe auflösen
                     groupMap.Remove(toGroup);
@@ -66,12 +82,60 @@ namespace GraphLibrary.Algorithm
                     mstGraph.AddEdge(from, to, new Dictionary<string, double>(edge.Costs));
                 }
 
-                
+
             }
 
             return mstGraph;
         }
 
+
+
+        public static IGraph Kruskal(IGraph graph, string costKey)
+        {
+            // Graph zusammenbauen
+            IGraph mstGraph = new Graph("MST of" + graph.Identifier, graph.IsDirected);
+
+            var disJointSet = new DisJointSet<string>();
+
+            // alle Knoten rein
+            foreach (var vertex in graph.Vertices)
+            {
+                mstGraph.AddVertex(vertex.Value.Identifier);
+                disJointSet.MakeSet(vertex.Key);
+            }
+
+
+
+            //sortierte Edges
+            var sortedEdges = graph.Edges.Values.OrderBy(x => x.Costs[costKey]);
+
+
+            //Durchgehen und einfügen, wenn kein Kreis entsteht im Baum
+            foreach (var edge in sortedEdges)
+            {
+                string fromId = edge.FromVertex.Identifier;
+                string toId = edge.ToVertex.Identifier;
+
+                //Wäre nun bei hinzunahme ein Kreis?
+                // Wenn man zwei Mengen nochmal verbindet, dann entstände ein Kreis
+
+                var setA = disJointSet.FindSet(fromId);
+                var setB = disJointSet.FindSet(toId);
+
+                //can't pick edge with both ends already in MST
+                if (!setA.Equals(setB))
+                {
+                    //union picked edge vertice sets
+                    disJointSet.Union(setA, setB);
+
+                    var from = mstGraph.Vertices[fromId];
+                    var to = mstGraph.Vertices[toId];
+                    mstGraph.AddEdge(from, to, new Dictionary<string, double>(edge.Costs));
+                }
+            }
+
+            return mstGraph;
+        }
 
 
 
@@ -88,7 +152,7 @@ namespace GraphLibrary.Algorithm
             foreach (var entry in graph.Vertices)
             {
                 // maximale Kosten eintragen
-                minCostsToVertexId[entry.Key] = int.MaxValue;
+                minCostsToVertexId[entry.Key] = double.MaxValue;
                 parent[entry.Key] = null;
             }
 
@@ -181,5 +245,290 @@ namespace GraphLibrary.Algorithm
 
         }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        class HeapElem : IComparable
+        {
+            public double Cost { get; set; }
+            public string ToVertexKey { get; set; }
+            
+
+            public int CompareTo(object other)
+            {
+                if (!(other is HeapElem))
+                {
+                    return -1;
+                }
+
+                return Cost.CompareTo(((HeapElem)other).Cost);
+            }
+        }
+
+
+        // Hier kommen leicht andere Werte bei den Kosten raus, also Funktioniert vermutlich nicht korrekt
+
+        public static IGraph PrimV2(IGraph graph, string costKey)
+        {
+            // Initialisierung der Merker für Vorgänger (Dictionary TO -> From für Edges später zu bilden)
+
+            // Minimale Kosten, um zu einer Edge zu kommen
+            FibornacciMinHeap<HeapElem> heap = new FibornacciMinHeap<HeapElem>();
+            Dictionary<string, FibornacciHeapNode<HeapElem>> fastAccess = new Dictionary<string, FibornacciHeapNode<HeapElem>>();
+
+            // Vorgänger
+            Dictionary<string, string> parent = new Dictionary<string, string>();
+
+            foreach (var entry in graph.Vertices)
+            {
+                // maximale Kosten eintragen
+                var node = heap.Insert(new HeapElem { Cost = double.MaxValue, ToVertexKey = entry.Key });
+                fastAccess[entry.Key] = node;
+                parent[entry.Key] = null;
+            }
+
+            // Kosten des Startknotens (einfach des ersten)
+            string first = graph.Vertices.First().Key;
+            var elem = fastAccess[first];
+            elem.Value.Cost = 0;
+            heap.DecrementKey(elem);
+
+
+
+            // alle Knoten, die noch zu besuchen/in MST zu übernehmen sind
+            Dictionary<string, IVertex> vertices = new Dictionary<string, IVertex>(graph.Vertices);
+            while (vertices.Count > 0)
+            {
+                var minObj = heap.ExtractMin();
+
+                IVertex obj = vertices[minObj.ToVertexKey];
+
+                // aus vertices raus
+                vertices.Remove(minObj.ToVertexKey);
+
+                // für alle Nachbarn
+                foreach (var neighbour in obj.Neighbours)
+                {
+                    // hole die entsprechende Kante
+                    var connectingEdge = graph.GetEdge(obj, neighbour.Value);
+
+                    var heapElem = fastAccess[neighbour.Key];
+
+                    // wenn neighbour noch in der Liste der nicht besuchten ist bzw. noch nicht im MST ist
+                    // und es ist noch besser als bisher von den Kosten her
+                    if (vertices.ContainsKey(neighbour.Key) && connectingEdge.Costs[costKey] < heapElem.Value.Cost)
+                    {
+                        // Vorgänger/Parent setzen
+                        parent[neighbour.Key] = minObj.ToVertexKey;
+
+                        // Kosten anpassen
+                        heapElem.Value.Cost = connectingEdge.Costs[costKey];
+                        heap.DecrementKey(heapElem);
+                    }
+                }
+            }
+
+
+            // Graph zusammenbauen
+            IGraph mstGraph = new Graph("MST of" + graph.Identifier, graph.IsDirected);
+
+            // alle Knoten rein
+            foreach (var vertex in graph.Vertices)
+            {
+                mstGraph.AddVertex(vertex.Value.Identifier);
+            }
+
+            //Nun die Kanten aus der Parent rausholen:
+            foreach (var entry in parent)
+            {
+                // startknoten hat null, also keinen Vorgänger
+                if (entry.Value != null)
+                {
+                    var fromInNew = mstGraph.Vertices[entry.Value];
+                    var toInNew = mstGraph.Vertices[entry.Key];
+
+                    var fromInOrig = graph.Vertices[entry.Value];
+                    var toInOrig = graph.Vertices[entry.Key];
+
+                    var origEdge = graph.GetEdge(fromInOrig, toInOrig);
+
+                    mstGraph.AddEdge(fromInNew, toInNew, new Dictionary<string, double>(origEdge.Costs));
+                }
+            }
+
+            return mstGraph;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public class MyNode : FastPriorityQueueNode
+        {
+            //Put custom properties here
+            public string Id { get; set; }
+        }
+
+
+
+        public static IGraph PrimV3(IGraph graph, string costKey)
+        {
+            // Initialisierung der Merker für Vorgänger (Dictionary TO -> From für Edges später zu bilden)
+
+            // Minimale Kosten, um zu einer Edge zu kommen
+            FastPriorityQueue<MyNode> q = new FastPriorityQueue<MyNode>(graph.Vertices.Count);
+            Dictionary<string, MyNode> fastAccess = new Dictionary<string, MyNode>();
+
+            // Vorgänger
+            Dictionary<string, string> parent = new Dictionary<string, string>();
+
+            foreach (var entry in graph.Vertices)
+            {
+                // maximale Kosten eintragen
+                var node = new MyNode { Id = entry.Key };
+                q.Enqueue(node, double.MaxValue);
+                fastAccess[entry.Key] = node;
+                parent[entry.Key] = null;
+            }
+
+            // Kosten des Startknotens (einfach des ersten)
+            string first = graph.Vertices.First().Key;
+            var elem = fastAccess[first];
+            q.UpdatePriority(elem, 0);
+
+
+
+            // alle Knoten, die noch zu besuchen/in MST zu übernehmen sind
+            Dictionary<string, IVertex> vertices = new Dictionary<string, IVertex>(graph.Vertices);
+            while (vertices.Count > 0)
+            {
+                var minObj = q.Dequeue();
+
+                IVertex obj = vertices[minObj.Id];
+
+                // aus vertices raus
+                vertices.Remove(minObj.Id);
+
+                // für alle Nachbarn
+                foreach (var neighbour in obj.Neighbours)
+                {
+                    // hole die entsprechende Kante
+                    var connectingEdge = graph.GetEdge(obj, neighbour.Value);
+
+                    var neighbourElem = fastAccess[neighbour.Key];
+
+                    // wenn neighbour noch in der Liste der nicht besuchten ist bzw. noch nicht im MST ist
+                    // und es ist noch besser als bisher von den Kosten her
+                    if (vertices.ContainsKey(neighbour.Key) && connectingEdge.Costs[costKey] < neighbourElem.Priority)
+                    {
+                        // Vorgänger/Parent setzen
+                        parent[neighbour.Key] = minObj.Id;
+
+                        // Kosten anpassen
+                        q.UpdatePriority(neighbourElem, connectingEdge.Costs[costKey]);
+                    }
+                }
+            }
+
+
+            // Graph zusammenbauen
+            IGraph mstGraph = new Graph("MST of" + graph.Identifier, graph.IsDirected);
+
+            // alle Knoten rein
+            foreach (var vertex in graph.Vertices)
+            {
+                mstGraph.AddVertex(vertex.Value.Identifier);
+            }
+
+            //Nun die Kanten aus der Parent rausholen:
+            foreach (var entry in parent)
+            {
+                // startknoten hat null, also keinen Vorgänger
+                if (entry.Value != null)
+                {
+                    var fromInNew = mstGraph.Vertices[entry.Value];
+                    var toInNew = mstGraph.Vertices[entry.Key];
+
+                    var fromInOrig = graph.Vertices[entry.Value];
+                    var toInOrig = graph.Vertices[entry.Key];
+
+                    var origEdge = graph.GetEdge(fromInOrig, toInOrig);
+
+                    mstGraph.AddEdge(fromInNew, toInNew, new Dictionary<string, double>(origEdge.Costs));
+                }
+            }
+
+            return mstGraph;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        class DoubleWrapper : IComparable
+        {
+            public double Val { get; set; }
+
+            public string Key { get; set; }
+
+            public int CompareTo(object obj)
+            {
+                return Val.CompareTo(((DoubleWrapper)obj).Val);
+            }
+        }
     }
 }
