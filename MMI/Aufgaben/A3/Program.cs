95b98f683b69c48153c727eec4171fbe45c08f13
Nils Schlicher <Nils.Schlicher@alumni.fh-aachen.de>
2018-05-09 18:42:45 +0200

    Anlage Aufgabe 3 und Implementierung von allen Touren ausprobieren

---
+++ MMI/Aufgaben/A3/Program.cs
@@ -0,0 +1,70 @@
+﻿using GraphLibrary.Algorithm;
+using GraphLibrary.Factory;
+using GraphLibrary.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var costName = "Kosten";
+
+
+            var s = File.ReadAllText(@"SampleData\K_10.txt");
+
+            IGraph gTest = GraphFactory.GraphFromAdjListStringWithCost(s, "T1", costName, false);
+
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            var bestWay = TSP.TryAllTours(gTest, costName);
+
+
+
+            //long cnt = 0;
+
+            //int n = 10;
+            //for (int i1 = 0; i1 < n; ++i1)
+            //    for (int i2 = 0; i2 < n-1; ++i2)
+            //        for (int i3 = 0; i3 < n-2; ++i3)
+            //            for (int i4 = 0; i4 < n-3; ++i4)
+            //                for (int i5 = 0; i5 < n-4; ++i5)
+            //                    for (int i6 = 0; i6 < n-5; ++i6)
+            //                        for (int i7 = 0; i7 < n-6; ++i7)
+            //                            for (int i8 = 0; i8 < n-7; ++i8)
+            //                                for (int i9 = 0; i9 < n-8; ++i9)
+            //                                    for (int i10 = 0; i10 < n-9; ++i10)
+            //                                        ++cnt;
+
+
+            //int n = 12;
+            //for (int i1 = 0; i1 < n; ++i1)
+            //    for (int i2 = 0; i2 < n - 1; ++i2)
+            //        for (int i3 = 0; i3 < n - 2; ++i3)
+            //            for (int i4 = 0; i4 < n - 3; ++i4)
+            //                for (int i5 = 0; i5 < n - 4; ++i5)
+            //                    for (int i6 = 0; i6 < n - 5; ++i6)
+            //                        for (int i7 = 0; i7 < n - 6; ++i7)
+            //                            for (int i8 = 0; i8 < n - 7; ++i8)
+            //                                for (int i9 = 0; i9 < n - 8; ++i9)
+            //                                    for (int i10 = 0; i10 < n - 9; ++i10)
+            //                                        for (int i11 = 0; i11 < n - 10; ++i11)
+            //                                            for (int i12 = 0; i12 < n - 11; ++i12)
+            //                                                ++cnt;
+
+
+            sw.Stop();
+
+            Console.WriteLine(bestWay.Sum(x => x.Costs[costName]));
+            Console.WriteLine($"{sw.ElapsedMilliseconds} ms");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MMI/GraphLibrary/Algorithm/TSP.cs
@@ -0,0 +1,132 @@
+﻿using GraphLibrary.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphLibrary.Algorithm
+{
+    /// <summary>
+    /// Funktionen zum Traveling Salesman Problem (TSP)
+    /// </summary>
+    public static class TSP
+    {
+
+        public static List<IEdge> NearestNeighbour()
+        {
+
+
+
+
+            return null;
+        }
+
+
+
+
+        public static List<IEdge> TryAllTours(IGraph graph, string costKey)
+        {
+            List<IEdge> bestWay = null;
+            double bestCost = double.MaxValue;
+
+
+            foreach (var vertex in graph.Vertices.Values)
+            {
+                List<IEdge> usedEdges = new List<IEdge>();
+                Dictionary<string, bool> seenVertices = new Dictionary<string, bool>();
+
+                foreach (var key in graph.Vertices.Keys)
+                {
+                    seenVertices.Add(key, false);
+                }
+
+                TryAllToursRecursion(graph, costKey, vertex, vertex, 0, seenVertices, usedEdges, ref bestWay, ref bestCost);
+            }
+
+            /*List<IEdge>[] bestWays = new List<IEdge>[graph.Vertices.Count];
+            double[] bestCosts = Enumerable.Repeat(double.MaxValue, graph.Vertices.Count).ToArray();
+
+            int cnt = -1;
+
+            Parallel.ForEach(graph.Vertices.Values, (vertex) => 
+            {
+                int localCnt = ++cnt;
+                List<IEdge> usedEdges = new List<IEdge>();
+                Dictionary<string, bool> seenVertices = new Dictionary<string, bool>();
+
+                foreach (var key in graph.Vertices.Keys)
+                {
+                    seenVertices.Add(key, false);
+                }
+
+                TryAllRecursion(graph, costKey, vertex, vertex, 0, seenVertices, usedEdges, ref bestWays[localCnt], ref bestCosts[localCnt]);
+            });
+
+
+            List<IEdge> bestWay = null;
+            foreach (var way in bestWays)
+            {
+                if (bestWay == null || way.Sum(x => x.Costs[costKey]) < bestWay.Sum(x => x.Costs[costKey]))
+                {
+                    bestWay = way;
+                }
+            }*/
+
+            return bestWay;
+        }
+
+
+        private static void TryAllToursRecursion(IGraph graph, string costKey, IVertex start, IVertex current, double currentCosts, Dictionary<string, bool> seenVertices, List<IEdge> usedEdges, ref List<IEdge> bestWay, ref double bestCost)
+        {
+            // current wurde nun besucht
+            seenVertices[current.Identifier] = true;
+
+            // Wenn alle gesehen, dann ist der Hamilton-Kreis komplett. Dazu n-1 Kanten. Die letzte zum Kreisschluss wird hier hinzugefügt
+            if (usedEdges.Count == graph.Vertices.Count - 1)
+            {
+                var closingEdge = graph.GetEdge(current, start);
+                usedEdges.Add(closingEdge);
+
+                // Erstes ist bis dahin bestes oder Kosten noch günstiger
+                double costToCheck = currentCosts + closingEdge.Costs[costKey];
+                if (bestWay == null || costToCheck < bestCost)
+                {
+                    bestWay = new List<IEdge>(usedEdges);
+                    bestCost = costToCheck;
+                }
+
+                usedEdges.RemoveAt(usedEdges.Count - 1);
+            }
+            else
+            {
+                // es geht noch weiter
+
+                // nehme eine Kante und gehe zu einem noch nicht besuchten
+                foreach (var edge in current.Edges.Values)
+                {
+                    // wurde der andere schon besucht?
+                    var other = edge.GetOtherVertex(current);
+                    if (seenVertices[other.Identifier] == false)
+                    {
+                        double newCosts = currentCosts + edge.Costs[costKey];
+
+                        //if ((bestWay == null || newCosts < bestCost)) //Branch and Bound
+                        {
+                            // Kante als genutzt vermerken
+                            usedEdges.Add(edge);
+                            TryAllToursRecursion(graph, costKey, start, other, newCosts, seenVertices, usedEdges, ref bestWay, ref bestCost);
+                            usedEdges.RemoveAt(usedEdges.Count - 1);
+                        }
+                    }
+
+                }
+            }
+
+            // und man verlässt current wieder
+            seenVertices[current.Identifier] = false;
+        }
+
+
+    }
+}
